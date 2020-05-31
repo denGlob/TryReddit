a91f7a24e03ab9a1c0b1e8edc1d236d4a1a9f7fe
Denys Smishny <denshiksmile@gmail.com>
2020-05-31 21:33:03 +0300

    - started architecture

---
+++ app/src/main/java/com/ua/tryreddit/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ua.tryreddit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ua.tryreddit.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
